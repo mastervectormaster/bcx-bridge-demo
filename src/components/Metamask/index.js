b4209b0dd4c130c6b48d2a975d0a307a4345edd7
Vectormaster <vectormaster123456789@gmail.com>
2022-11-17 02:59:51 -0600

    bridging working

--- src/components/Metamask/index.js
+++ src/components/Metamask/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import BridgeAbi from '../../abi/Bridge.json'
+import BcxAbi from '../../abi/MockBcx.json'
 
 import { ethers } from "ethers";
 
@@ -7,8 +9,22 @@ const Metamask = () => {
 
   const connectToMetamask = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
     const accounts = await provider.send("eth_requestAccounts", []);
     setAddresss(accounts[0]);
+    
+    const bcxContractAddr = process.env.REACT_APP_BCX_CONTRACT_ADDR;
+    const bridgeContractAddr = process.env.REACT_APP_BRIDGE_CONTRACT_ADDR;
+
+    const bcxContract = new ethers.Contract(bcxContractAddr, BcxAbi, signer);
+
+    const name = await bcxContract.name()
+
+    await bcxContract.approve(bridgeContractAddr, 10000);
+    
+    const bridgeContract = new ethers.Contract(bridgeContractAddr, BridgeAbi, signer);
+  
+    await bridgeContract.sendToCosmos(bcxContractAddr, 'cosmos123', 10000)
   };
 
   const renderMetamask = () => {
