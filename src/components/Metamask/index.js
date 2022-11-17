164340f2a6930fe7c6a6aeebb3ef417c6181d015
Vectormaster <vectormaster123456789@gmail.com>
2022-11-17 04:03:45 -0600

    frontend improved

--- src/components/Metamask/index.js
+++ src/components/Metamask/index.js
@@ -1,46 +1,149 @@
 import React, { useState } from "react";
-import BridgeAbi from '../../abi/Bridge.json'
-import BcxAbi from '../../abi/MockBcx.json'
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 
 import { ethers } from "ethers";
 
+import BridgeAbi from "../../abi/Bridge.json";
+import BcxAbi from "../../abi/MockBcx.json";
+
+const BCX_CONTRACT_ADDR = process.env.REACT_APP_BCX_CONTRACT_ADDR;
+
+const BRIDGE_CONTRACT_ADDR = process.env.REACT_APP_BRIDGE_CONTRACT_ADDR;
+
 const Metamask = () => {
-  const [address, setAddresss] = useState('');
+  const [address, setAddresss] = useState("");
+  const [provider, setProvider] = useState(null);
+  const [formData, setFormData] = useState({
+    addr: "",
+    amount: 0,
+  });
 
-  const connectToMetamask = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const accounts = await provider.send("eth_requestAccounts", []);
-    setAddresss(accounts[0]);
-    
-    const bcxContractAddr = process.env.REACT_APP_BCX_CONTRACT_ADDR;
-    const bridgeContractAddr = process.env.REACT_APP_BRIDGE_CONTRACT_ADDR;
-
-    const bcxContract = new ethers.Contract(bcxContractAddr, BcxAbi, signer);
-
-    const name = await bcxContract.name()
-
-    await bcxContract.approve(bridgeContractAddr, 10000);
-    
-    const bridgeContract = new ethers.Contract(bridgeContractAddr, BridgeAbi, signer);
-  
-    await bridgeContract.sendToCosmos(bcxContractAddr, 'cosmos123', 10000)
+  const shortAddr = (fullAddr) => {
+    return fullAddr.slice(0, 10) + "...";
   };
 
-  const renderMetamask = () => {
+  const connectToMetamask = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // const signer = provider.getSigner();
+      const accounts = await provider.send("eth_requestAccounts", []);
+      setAddresss(accounts[0]);
+      setProvider(provider);
+    } catch (e) {
+      alert("MetaMask not installed");
+    }
+
+    // const bcxContract = new ethers.Contract(BCX_CONTRACT_ADDR, BcxAbi, signer);
+
+    // await bcxContract.approve(BRIDGE_CONTRACT_ADDR, 10000);
+
+    // const bridgeContract = new ethers.Contract(
+    //   BRIDGE_CONTRACT_ADDR,
+    //   BridgeAbi,
+    //   signer
+    // );
+
+    // await bridgeContract.sendToCosmos(BCX_CONTRACT_ADDR, "cosmos123", 10000);
+  };
+
+  const handleTransfer = async (e) => {
+    e.preventDefault();
+    try {
+      const signer = provider.getSigner();
+      const bcxContract = new ethers.Contract(
+        BCX_CONTRACT_ADDR,
+        BcxAbi,
+        signer
+      );
+
+      await bcxContract.approve(BRIDGE_CONTRACT_ADDR, 10000);
+
+      const bridgeContract = new ethers.Contract(
+        BRIDGE_CONTRACT_ADDR,
+        BridgeAbi,
+        signer
+      );
+
+      await bridgeContract.sendToCosmos(BCX_CONTRACT_ADDR, "cosmos123", 10000);
+    } catch (e) {
+      alert("MetaMask not installed");
+    }
+  };
+
+  const connectMetamask = () => {
     if (!address) {
       return (
-        <button onClick={() => connectToMetamask()}>Connect to Metamask</button>
-      )
-    } else {
-      return (
-        <div>
-          <p>Welcome {address}</p>
-        </div>
+        <Button className="w-100" onClick={() => connectToMetamask()}>
+          Connect to Metamask
+        </Button>
       );
+    } else {
+      return <div className="text-center">{shortAddr(address)}</div>;
     }
-  }
-  return <div>{renderMetamask()}</div>;
+  };
+
+  const handleChange = (e) => {
+    setFormData((formData) => ({ ...formData, [e.target.id]: e.target.value }));
+  };
+
+  return (
+    <Container>
+      <Row className="my-5">
+        <Col md={{ span: 10 }}>
+          <h1 className="text-center">
+            BCX Token from Polygon Mumbai to Blockx
+          </h1>
+        </Col>
+        <Col md={{ span: 2 }}>{connectMetamask()}</Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 10, offset: 1 }}>
+          {address !== "" && (
+            <Form>
+              <Form.Group className="mb-3" controlId="addr">
+                <Form.Label>
+                  Recipient Blockx Account (e.g.
+                  blockx14gct2nk2d9f6rs0edsyg4nuavkng8t8u0qf6dw)
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Blockx Address"
+                  value={formData.addr}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="amount">
+                <Form.Label>Amount to transfer (in abcx, NOT bcx)</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Amount in abcx"
+                  value={formData.amount}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Row className="mt-5">
+                <Col md={{ span: 4, offset: 4 }}>
+                  <Button
+                    variant="primary"
+                    className="w-100"
+                    onClick={handleTransfer}
+                  >
+                    Transfer
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Metamask;
